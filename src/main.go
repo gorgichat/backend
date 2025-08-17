2d2a67f0ae33a312d88740e290debafe40a25b69
lazypwny751 <testfordnshijacking@gmail.com>
2025-08-17 21:58:08 +0300

    cli parameters

--- src/main.go
+++ src/main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
+	"os"
+	"flag"
+	"strconv"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log/slog"
 )
 
+var (
+	host = flag.String("host", "127.0.0.1", "Gorgi Chat host")
+	port = flag.Int("port", 8080, "Gorgi Chat running port")
+
+	dbHost     = flag.String("db-host", "localhost", "Database host")
+	dbPort     = flag.Int("db-port", 3306, "Database port")
+	dbUser     = flag.String("db-user", "root", "Database user")
+	dbPassword = flag.String("db-password", "", "Database password")
+	dbName     = flag.String("db-name", "gorgi_chat", "Database name")
+)
+
 func main() {
+	flag.Parse()
+
 	err := godotenv.Load()
 	if err != nil {
 		slog.Error("Error loading .env file")
+	}
+
+	if val := os.Getenv("HOST"); val != "" {
+		*host = val
+	}
+	if val := os.Getenv("PORT"); val != "" {
+		if parsed, err := strconv.Atoi(val); err == nil {
+			*port = parsed
+		}
+	}
+	if val := os.Getenv("DB_HOST"); val != "" {
+		*dbHost = val
+	}
+	if val := os.Getenv("DB_PORT"); val != "" {
+		if parsed, err := strconv.Atoi(val); err == nil {
+			*dbPort = parsed
+		}
+	}
+	if val := os.Getenv("DB_USER"); val != "" {
+		*dbUser = val
+	}
+	if val := os.Getenv("DB_PASSWORD"); val != "" {
+		*dbPassword = val
+	}
+	if val := os.Getenv("DB_NAME"); val != "" {
+		*dbName = val
 	}
 
 	r := gin.Default()
